f57d62bc41fb1910734d0355b8f46e7213a7a65c
Prateek Roy <royprateek96@gmail.com>
2021-09-21 02:41:41 +0530

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,44 +31,44 @@ export class AppComponent {
 
   ngOnInit() {
     this.titleService.setTitle(this.title);
-    this.metaService.addTags([
-      { name: 'keywords', content: 'Angular, Universal, Portfolio, Frontend' },
-      { name: 'description', content: 'Full Stack Portfolio' },
-      { name: 'author', content: 'Prateek Roy' },
-      { name: 'robots', content: 'index, follow' },
+    // this.metaService.addTags([
+    //   { name: 'keywords', content: 'Angular, Universal, Portfolio, Frontend' },
+    //   { name: 'description', content: 'Full Stack Portfolio' },
+    //   { name: 'author', content: 'Prateek Roy' },
+    //   { name: 'robots', content: 'index, follow' },
 
-      // Twitter
-      { name: 'twitter:card', content: 'summary_large_image' },
-      { name: 'twitter:site', content: '' },
-      { name: 'twitter:creator', content: '@royprateek96' },
-      {
-        name: 'twitter:title',
-        content: "Prateek's Portfolio",
-      },
-      {
-        name: 'twitter:description',
-        content: 'Hey! My name is Prateek and this is my portfolio.',
-      },
-      {
-        name: 'twitter:image',
-        content: '/assets/images/thumbnail.png',
-      },
+    //   // Twitter
+    //   { name: 'twitter:card', content: 'summary_large_image' },
+    //   { name: 'twitter:site', content: '' },
+    //   { name: 'twitter:creator', content: '@royprateek96' },
+    //   {
+    //     name: 'twitter:title',
+    //     content: "Prateek's Portfolio",
+    //   },
+    //   {
+    //     name: 'twitter:description',
+    //     content: 'Hey! My name is Prateek and this is my portfolio.',
+    //   },
+    //   {
+    //     name: 'twitter:image',
+    //     content: '/assets/images/thumbnail.png',
+    //   },
 
-      //LinkedIN
-      { property: 'og:title', content: "Prateek's Portfolio" },
-      {
-        property: 'og:image',
-        content: '/assets/images/thumbnail.png',
-      },
-      {
-        property: 'og:description',
-        content: 'Hey! My name is Prateek and this is my portfolio.',
-      },
-      {
-        property: 'og:url',
-        content: 'https://prateekroy96.github.io/portfolio/',
-      },
-    ]);
+    //   //LinkedIN
+    //   { property: 'og:title', content: "Prateek's Portfolio" },
+    //   {
+    //     property: 'og:image',
+    //     content: '/assets/images/thumbnail.png',
+    //   },
+    //   {
+    //     property: 'og:description',
+    //     content: 'Hey! My name is Prateek and this is my portfolio.',
+    //   },
+    //   {
+    //     property: 'og:url',
+    //     content: 'https://prateekroy96.github.io/portfolio/',
+    //   },
+    // ]);
   }
   @HostListener('window:scroll', ['$event'])
   scrollHandler(event) {
